e480413bdfa8dd63e1bdc156aeba6713dee4844f
Madhavi Singh <01singh.madhavi@gmail.com>
2025-01-07 13:38:48 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -22,8 +22,9 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-// const __dirname = path.resolve();
-
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const __dirname = path.resolve();
+console.log(`Server running in ${NODE_ENV} mode on port ${PORT}`);
 app.use(express.json({ limit: "5mb" })); // to parse req.body
 // limit shouldn't be too high to prevent DOS
 app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
@@ -35,13 +36,13 @@ app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "/frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-//   });
-// }
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
