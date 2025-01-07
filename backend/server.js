ea5c69e7c278704f2fe39087f76ad2f01f243f63
Madhavi Singh <01singh.madhavi@gmail.com>
2025-01-07 13:50:53 +0530

    change

--- backend/server.js
+++ backend/server.js
@@ -22,9 +22,9 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const NODE_ENV = process.env.NODE_ENV || 'development';
+// const NODE_ENV = process.env.NODE_ENV || 'development';
 const __dirname = path.resolve();
-console.log(`Server running in ${NODE_ENV} mode on port ${PORT}`);
+//console.log(`Server running in ${NODE_ENV} mode on port ${PORT}`);
 app.use(express.json({ limit: "5mb" })); // to parse req.body
 // limit shouldn't be too high to prevent DOS
 app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
